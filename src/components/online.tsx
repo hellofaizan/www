b3d1e97772d04b0a09fe56903b8544359ec1000d
Mohammad Faizan <faizancurious@gmail.com>
2024-09-15 10:22:33 +0530

    minor change to online status

--- src/components/online.tsx
+++ src/components/online.tsx
@@ -23,9 +23,9 @@ const Online = () => {
   return (
     <Suspense fallback={<div></div>}>
       {online && (
-        <span className="w-3 h-3 md:mt-3" title={data?.data?.discord_status}>
-          <span className="absolute bottom-0 right-0 md:right-3 md:bottom-3 bg-green-500 rounded-full h-5 w-5 border-4 animate-ping" />
-          <span className="absolute bottom-0 right-0 md:right-3 md:bottom-3 bg-green-500 rounded-full h-5 w-5 border-4" />
+        <span className="w-3 h-3" title={data?.data?.discord_status}>
+          <span className="absolute bottom-0 right-0 md:right-3 md:bottom-3 bg-green-500 rounded-full h-5 w-5 border-4 border-[#1d181f] animate-ping" />
+          <span className="absolute bottom-0 right-0 md:right-3 md:bottom-3 bg-green-500 rounded-full h-5 w-5 border-4 border-[#1d181f]" />
         </span>
       )}
     </Suspense>
