6e8d75ef3ec642489358f6e738a082b21de6edf3
Mohammad Faizan <faizancurious@gmail.com>
2024-10-01 15:14:46 +0530

    made code block better

--- src/lib/mdxtohtml.ts
+++ src/lib/mdxtohtml.ts
@@ -1,24 +1,32 @@
-import { unified } from 'unified';
-import remarkParse from 'remark-parse';
-import remarkRehype from 'remark-rehype';
-import rehypeStringify from 'rehype-stringify';
-import rehypePrettyCode from 'rehype-pretty-code';
-import { transformerCopyButton } from '@rehype-pretty/transformers';
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeAutoLinkHeadings from "rehype-autolink-headings";
+import { transformerCopyButton } from "@rehype-pretty/transformers";
 
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
-    .use(remarkParse)
-    .use(remarkRehype, { allowDangerousHtml: true })
-    .use(rehypePrettyCode as any, {
-      theme: 'catppuccin-macchiato',
+    .use(remarkParse as any)
+    .use(remarkRehype as any, { allowDangerousHtml: true })
+    .use(rehypePrettyCode, {
+      theme: "github-dark",
       transformers: [
         transformerCopyButton({
-          visibility: 'always',
+          visibility: "always",
           feedbackDuration: 3000,
         }),
       ],
     })
     .use(rehypeStringify as any)
+    .use(rehypeAutoLinkHeadings, {
+      behavior: "wrap",
+      properties: {
+        className: ["subheading-anchor"],
+        arialLabel: "Link to section",
+      },
+    })
     .process(markdown);
 
   return String(result);
