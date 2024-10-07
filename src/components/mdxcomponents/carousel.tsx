76d1751b54132ab016da913da477e316532d52e4
Mohammad Faizan <faizancurious@gmail.com>
2024-10-07 13:44:24 +0530

    carousel fix

--- src/components/mdxcomponents/carousel.tsx
+++ src/components/mdxcomponents/carousel.tsx
@@ -12,15 +12,17 @@ export default function Carousel({ children }: CarouselProps) {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000); // Change image every 3 seconds
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % (images.length * 2)); // Repeat images
+    }, 5000); // Change image every 5 seconds
 
     return () => clearInterval(interval); // Cleanup on unmount
   }, [images.length]);
 
   const handleImageClick = (index: number) => {
-    setCurrentIndex(index);
+    setCurrentIndex(index % (images.length * 2)); // Adjust index for infinite loop
   };
+
+  console.log(currentIndex);
 
   return (
     <div className="group overflow-hidden rounded-xl">
@@ -28,18 +30,20 @@ export default function Carousel({ children }: CarouselProps) {
         ref={carouselRef}
         className="flex transition-transform duration-500"
         initial={{ x: 0 }}
-        animate={{ x: `-${currentIndex * 90}%` }} // Move based on current index
+        animate={{ x: `-${currentIndex * 90}% ` }} // Move based on current index
         transition={{ duration: 0.5, ease: "easeInOut" }} // Smooth transition
       >
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className="flex-shrink-0 w-[90%]"
-            onClick={() => handleImageClick(index)}
-          >
-            {image}
-          </div>
-        ))}
+        {Array.from({ length: 2 }, (_, i) => i).flatMap((_) =>
+          images.map((image, index) => (
+            <div
+              key={index + images.length * _} // Adjust key for repeated images
+              className="flex-shrink-0 w-[90%]" // Added gap
+              onClick={() => handleImageClick(index + images.length * _)} // Adjust index for click
+            >
+              {image}
+            </div>
+          ))
+        )}
       </motion.div>
     </div>
   );
